296902521b34d1f98dd83957cb92d87261c054c2
Ontu <73860587+0ntu@users.noreply.github.com>
2024-11-27 10:32:42 -0500

    Basic Layout

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,70 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
+
 using namespace ftxui;
 
-int main(void) {
-  Element document = text("DiscoveryTree: Hello World!");
- 
-  auto screen = Screen::Create(
-    Dimension::Full(),       // Width
-    Dimension::Fit(document) // Height
-  );
-  Render(screen, document);
-  screen.Print();
- 
-  return EXIT_SUCCESS;
+int main() {
+  std::vector<std::string> tab_values{
+      "Browse",
+      "Suggested",
+      "Settings",
+      "Visualization",
+  };
+  int tab_selected = 0;
+  auto tab_toggle = Toggle(&tab_values, &tab_selected);
+
+  std::vector<std::string> tab_1_entries{
+      "Forest",
+      "Water",
+      "I don't know",
+  };
+  int tab_1_selected = 0;
+
+  std::vector<std::string> tab_2_entries{
+      "Hello",
+      "Hi",
+      "Hay",
+  };
+  int tab_2_selected = 0;
+
+  std::vector<std::string> tab_3_entries{
+      "Table",
+      "Nothing",
+      "Is",
+      "Empty",
+  };
+  int tab_3_selected = 0;
+  auto tab_container = Container::Tab(
+      {
+          Radiobox(&tab_1_entries, &tab_1_selected),
+          Radiobox(&tab_2_entries, &tab_2_selected),
+          Radiobox(&tab_3_entries, &tab_3_selected),
+      },
+      &tab_selected);
+
+  auto container = Container::Vertical({
+      tab_toggle,
+      tab_container,
+  });
+
+  auto text = ftxui::text("DiscoveryTree");
+
+  auto renderer = Renderer(container, [&] {
+    return vbox({
+               hflow(text),
+               separator(),
+               tab_toggle->Render(),
+               separator(),
+               tab_container->Render(),
+           }) |
+           border;
+  });
+
+  auto screen = ScreenInteractive::Fullscreen();
+  screen.Loop(renderer);
 }
