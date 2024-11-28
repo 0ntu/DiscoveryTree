7ed894227da797c7e89fc55a91f14d3b17594966
Ontu <73860587+0ntu@users.noreply.github.com>
2024-11-28 12:22:38 -0500

    Cmake add curl deps & nix build

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "DiscoveryTree Flake";
+  inputs = {nixpkgs.url = "github:nixos/nixpkgs/24.05";};
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+  in {
+    packages."${system}".default = with import nixpkgs {inherit system;};
+      stdenv.mkDerivation {
+        name = "DiscoveryTree";
+        buildInputs = with pkgs; [cmake make];
+        src = self;
+        buildPhase = ''
+            cmake .
+            make
+        '';
+        installPhase = ''
+          cp DiscoveryTree $out
+        '';
+      };
+
+    devShells."${system}".default = pkgs.mkShell {
+      buildInputs = with pkgs; [cmake make curl];
+    };
+  };
+}
