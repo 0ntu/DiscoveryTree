0d2684f9bc80a05df36d35733162bb8bf67fbf6d
Granache <dcfallon04@gmail.com>
2024-12-02 17:11:38 -0500

    added BPlusTree namespace for easier development of b_plus.cpp

--- src/b_plus.cpp
+++ src/b_plus.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "b_plus.h"
+namespace BPlusTree{
+    void createTree(vector<BooksFetcher::Book> books) {
+
+    }
+
+    void sortBooks(vector<BooksFetcher::Book>& books) {
+
+    }
+
+    void createLeafs(vector<BooksFetcher::Book>& books) {
+
+    }
+
+    void createInternalNodes(vector<BooksFetcher::Book>& books) {
+
+    }
+
+    b_plus::node* searchTree(int isbn) {
+
+    }
+}
--- src/b_plus.h
+++ src/b_plus.h
@@ -6,30 +6,32 @@
 using BooksFetcher::Book;
 using namespace std;
 
-class b_plus{
-public:
-    struct node{
-        bool isLeaf;
-        //Our keys are a vector of Book structs defined in fetcher.h
-        vector<Book> keys;
-        vector<node*> children;
-        node* next;
+namespace BPlusTree {
+    class b_plus {
+    public:
+        struct node {
+            bool isLeaf;
+            //Our keys are a vector of Book structs defined in fetcher.h
+            vector<Book> keys;
+            vector<node *> children;
+            node *next;
+        };
+        node *root;
+        int max_degree;
+
+        //Function to create a tree using a vector of books
+        void createTree(vector<Book> books);
+
+        //Function to sort a vector of books according to ISBN number
+        void sortBooks(vector<Book> &books);
+
+        //Function to create the leaf nodes
+        void createLeafs(vector<Book> &books);
+
+        //Function to create internal nodes
+        void createInternalNodes(vector<Book> &books);
+
+        //Function to search tree according to ISBN number
+        node *searchTree(int isbn);
     };
-    node* root;
-    int max_degree;
-
-    //Function to create a tree using a vector of books
-    void createTree(vector<Book> books);
-
-    //Function to sort a vector of books according to ISBN number
-    void sortBooks(vector<Book>& books);
-
-    //Function to create the leaf nodes
-    void createLeafs(vector<Book>& books);
-
-    //Function to create internal nodes
-    void createInternalNodes(vector<Book>& books);
-
-    //Function to search tree according to ISBN number
-    node* searchTree(int isbn);
-};
+}
