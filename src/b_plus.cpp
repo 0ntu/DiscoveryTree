994269125dc7031767c6e070cdbcf7072742cec1
Granache <dcfallon04@gmail.com>
2024-12-03 22:59:27 -0500

    added comments to newly implemented functions

--- src/b_plus.cpp
+++ src/b_plus.cpp
@@ -5,18 +5,24 @@
 #include "b_plus.h"
 #include <iostream>
 
+    // Destructor for the b+ tree
     b_plus::~b_plus() {
         int num_deleted = 0;
+
+        // Calls the deleteTree function to recursively delete the nodes
         deleteTree(root, num_deleted);
         cout << num_deleted << endl;
     }
 
+    // Function to recursively delete the nodes in the tree with postorder traversal
     void b_plus::deleteTree(b_plus::node *start_node, int& num_deleted) {
+        // If our current node is a leaf node, delete it and return
         if(start_node->isLeaf){
             delete start_node;
             num_deleted += 1;
             return;
         }
+        // If our current node is not a leaf node, visit all of its children first and delete them, then delete this node
         for(int i = 0; i < start_node->children.size(); i++){
             deleteTree(start_node->children[i], num_deleted);
         }
@@ -24,9 +30,15 @@
         delete start_node;
     }
 
+    // Function to create the tree given a vector of book objects
     void b_plus::createTree(vector<BooksFetcher::Book> books) {
+        // Sorts the vector of book objects
         sortBooks(books);
+
+        // Creates the leaves first
         createLeafs(books);
+
+        // Creates the internal nodes and sets the root
         createInternalNodes(books);
     }
 
@@ -106,33 +118,55 @@
 //        std::cout << books_added << std::endl;
     }
 
+    // This function creates the internal nodes of the b+ tree after the leaf nodes have been initialized
     void b_plus::createInternalNodes(vector<BooksFetcher::Book>& books) {
+        //  We start with our first layer, which connects to the leaf nodes
         vector<node*> first_layer;
         node* current_node = first_leaf;
+
+        // If there aren't enough leaves to create a full set of children for the internal node, we store what leaves
+        // there are as children in a single internal node
         if(num_nodes / order == 0){
             node* new_internal = new node;
             for(int i = 0; i < num_nodes; i++){
+                // If the current leaf is not the final leaf, we add the book with the smallest ISBN13 number in the
+                // next leaf to the keys of the internal node
                 if(i != num_nodes - 1){
                     new_internal->keys.push_back(current_node->next->keys[0]);
                 }
+
+                // We add the current leaf to the vector of children associated with the internal node
                 new_internal->children.push_back(current_node);
                 current_node = current_node->next;
             }
+            // We set the root to be the internal node
             root = new_internal;
         }
+        // Else, if there are enough leaves to create at least a single full set of children for an internal node,
+        // we loop for the number of full sets that can be made
         else{
             for(int i = 0; i < num_nodes / order; i++){
                 node* new_internal = new node;
                 first_layer.push_back(new_internal);
+
+                // If we are at the last full set of leaves, but there are additional leaves that cannot form a full set,
+                // we split the remaining leaves in half and create two new internal nodes
+                // This ensures that both internal nodes have at least ceil(order/2) children
                 if(i == (num_nodes / order) - 1 && num_nodes % order != 0){
                     int remaining_nodes = order + (num_nodes % order);
+
                     for(int j = 0; j < remaining_nodes / 2; j++){
                         if(j != (remaining_nodes / 2) - 1){
                             new_internal->keys.push_back(current_node->next->keys[0]);
                         }
+
+                        // We add the current leaf to the vector of children associated with the internal node
                         new_internal->children.push_back(current_node);
                         current_node = current_node->next;
                     }
+
+                    // We initialize the final node and add the remaining leaves to its children and any relevant
+                    // books to its keys
                     node* final_internal = new node;
                     first_layer.push_back(final_internal);
                     for(int k = 0; k < remaining_nodes - (remaining_nodes / 2); k++){
@@ -143,6 +177,9 @@
                         current_node = current_node->next;
                     }
                 }
+
+                // Else, if we can still make more full sets of leaves without any remainders following it,
+                // we add a full internal node as usual
                 else{
                     for(int j = 0; j < order; j++){
                         if(j != order - 1){
@@ -153,44 +190,71 @@
                     }
                 }
             }
+
+            // Once the first layer of internal nodes has been initialized, we call fillInternalNodes to create more
+            // internal nodes leading to the root
             fillInternalNodes(first_layer);
         }
     }
 
+    // This function takes in the first layer of internal nodes and builds up from it until a single root node is reached
     void b_plus::fillInternalNodes(vector<b_plus::node *>& start_layer) {
+        // current_layer is the current layer that is being filled with nodes that point to the nodes in start_layer
         vector<node*> current_layer;
+
+        // While there is still more than one node in the starting layer, we create an additional layer of
+        // internal nodes to point to the starting layer
         while(start_layer.size() != 1){
+
+            // If there is not enough nodes in the starting layer to create a full set of children, we place the
+            // remainder within a single internal node
             if(start_layer.size() / order == 0){
                 node* new_internal = new node;
                 current_layer.push_back(new_internal);
+
                 for(int i = 0; i < start_layer.size(); i++){
                     if(i != start_layer.size() - 1){
                         new_internal->keys.push_back(getSmallestBook(start_layer[i + 1]));
                     }
+
                     new_internal->children.push_back(start_layer[i]);
                 }
             }
+
+            // Else, if there are enough nodes to create at least one full set of children,
+            // we create as many full internal nodes as possible
             else{
                 for(int i = 0; i < start_layer.size() / order; i++){
                     node* new_internal = new node;
                     current_layer.push_back(new_internal);
+
+                    // If we are at the final set of full nodes but there are still remaining nodes after it,
+                    // we combine the remaining nodes and split them among two internal nodes
                     if(i == (start_layer.size() / order) - 1 && start_layer.size() % order != 0){
                         int remaining_nodes = order + (start_layer.size() % order);
+
                         for(int j = 0; j < remaining_nodes / 2; j++){
                             if(j != (remaining_nodes / 2) - 1){
                                 new_internal->keys.push_back(getSmallestBook(start_layer[(i * order) + j + 1]));
                             }
+
                             new_internal->children.push_back(start_layer[(i * order) + j]);
                         }
+
                         node* final_internal = new node;
                         current_layer.push_back(final_internal);
+
                         for(int k = 0; k < remaining_nodes - (remaining_nodes / 2); k++){
                             if(k != remaining_nodes - (remaining_nodes / 2) - 1){
                                 final_internal->keys.push_back(getSmallestBook(start_layer[(i * order) + k + (remaining_nodes / 2) + 1]));
                             }
+
                             final_internal->children.push_back(start_layer[(i * order) + k + (remaining_nodes / 2)]);
                         }
                     }
+
+                    // Else, if there are still more full sets of nodes without any remainders after them,
+                    // we create full internal nodes as usual
                     else{
                         for(int j = 0; j < order; j++){
                             if(j != order - 1){
@@ -201,12 +265,17 @@
                     }
                 }
             }
+            // Once every node in the start_layer have become children to nodes in the current_layer,
+            // we set the start_layer to the current_layer, clear the current_layer, and repeat
             start_layer = current_layer;
             current_layer.clear();
         }
+
+        // Once the start_layer has only one node, we set that node to be the root
         root = start_layer[0];
     }
 
+    // This function gets the book with the smallest ISBN13 number contained within the children of a given node
     Book b_plus::getSmallestBook(b_plus::node *start_node) {
         while(!start_node->isLeaf){
             start_node = start_node->children[0];
--- src/b_plus.h
+++ src/b_plus.h
@@ -18,6 +18,8 @@ using namespace std;
         node *root;
         node *first_leaf;
         int num_nodes = 0;
+        int order;
+
 
         // The order defines the maximum number of children each internal node can have
         // Each internal node has at least ceil(order/2) children and at most order-1 keys
@@ -27,26 +29,27 @@ using namespace std;
 
         ~b_plus();
 
-        int order;
-
-        //Function to create a tree using a vector of books
+        // Function to create a tree using a vector of books
         void createTree(vector<Book> books);
 
-        //Function to sort a vector of books according to ISBN13 number
+        // Function to sort a vector of books according to ISBN13 number
         void sortBooks(vector<Book> &books);
 
-        //Function to create the leaf nodes
+        // Function to create the leaf nodes
         void createLeafs(vector<Book> &books);
 
-        //Function to create internal nodes
+        // Function to create the internal nodes
         void createInternalNodes(vector<Book> &books);
 
+        // Function to fill in the remaining layers of the internal nodes
         void fillInternalNodes(vector<node*>& start_layer);
 
+        // Function to get the smallest book in a subsection of the b+ tree
         Book getSmallestBook(node* start_node);
 
+        // Function to delete the tree using postorder traversal
         void deleteTree(node* start_node, int& num_deleted);
 
-        //Function to search tree according to ISBN13 number
+        // Function to search tree according to ISBN13 number
         node *searchTree(int isbn);
     };
