8274b917f25ee5a00e4badedf2de2010fd579828
Ontu <73860587+0ntu@users.noreply.github.com>
2024-12-04 18:47:57 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+# Build, run, and package DiscoveryTree declaratively
 {
   description = "DiscoveryTree Flake";
   inputs = {nixpkgs.url = "github:nixos/nixpkgs/24.05";};
@@ -8,8 +9,8 @@
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-  in {
-    packages."${system}".default = with import nixpkgs {inherit system;};
+
+    deriv = with import nixpkgs {inherit system;};
       stdenv.mkDerivation {
         name = "DiscoveryTree";
         buildInputs = with pkgs; [cmake ftxui];
@@ -22,11 +23,21 @@
         installPhase = ''
           mkdir -p $out/bin
           mkdir -p $out/share/discoverytree
-          cp books.csv $out/share/discoverytree
+          cp books.csv $out/bin/books.csv
+          cp books.csv $out/share/discoverytree/books.csv
           cp DiscoveryTree $out/bin/DiscoveryTree
         '';
       };
+  in {
+    apps."${system}".default = {
+        type = "app";
+        program = "${deriv}/bin/DiscoveryTree";
+        args = ["${deriv}/share/discoverytree/books.csv"];
+      };
 
+    packages."${system}".default = deriv;
+
+    #Development environment toolkit
     devShells."${system}".default = pkgs.mkShell {
       buildInputs = with pkgs; [cmake curl ftxui];
     };
